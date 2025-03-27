30181911b9af4df8d9ffd043fc0c36bb8abd0a07
Hayford sammor-Duah <psam4ord@gmail.com>
2025-03-27 13:46:52 +0000

    new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import * as React from "react"
 import MainPage from "./homepage"
-import Layout from '../components/layout/LayOut.jsx'
+import Layout from "../components/layout/LayOut.jsx"
 import ContactInfo from "../components/helpers/ContactInfo.jsx"
 import Seo from "../components/seo"
+import AboutPage from "./about.js"
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const IndexPage = () => {
-  return ( 
-
+  return (
 
     <>
-    {/* <Vision /> */}
-    < ContactInfo />
+    <AboutPage />
     </>
-  //    <Layout> 
-  //  <MainPage />
-  //  </Layout>
+    // <Layout>
+    //   <MainPage />
+    // </Layout>
   )
 }
 
